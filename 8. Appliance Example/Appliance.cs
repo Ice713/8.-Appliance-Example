cb7a689b75a544385dbe9ac72da19fdb9f4fd53c
Ice713 <icramzilruiz@gmail.com>
2025-10-20 10:18:22 +0800

    Add Appliance and WashingMachine classes
    Updated `Appliance Example.csproj` to include new source files: `Appliance.cs` and `WashingMachine.cs`. The `Appliance.cs` file introduces a class `Appliance` with a `TurnOn()` method, while `WashingMachine.cs` defines a class `WashingMachine` with no methods or properties at this time.

---
+++ 8. Appliance Example/Appliance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.Appliance_Example
+{
+    internal class Appliance
+    {
+        public string TurnOn()
+        {
+            return "Appliance is ON";
+        }
+    }
+}
